b85524860730b0bddd528523c79ca8ab01079ed9
mhiorns-moj <martin.hiorns@justice.gov.uk>
2026-04-20 17:17:29 +0100

    Typescript config updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,12 @@
 import { defineConfig, devices } from '@playwright/test'
-import { testEnvironment } from 'localSettings'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: 'tests',
   testMatch: '**/*.test.ts',
   fullyParallel: false,
   workers: 1,
   timeout: 600_000,
-  // maxFailures: 1,
 
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { open: 'never', noSnippets: false }]],
 
   use: {
